3d831a9efb39b41a23d10456e5e91465521f8d1e
Ilya Khlobystov <titaniumidk@gmail.com>
2020-03-23 17:06:52 +0300

    Initial Commit

---
+++ StocksApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StocksApp
+//
+//  Created by Ilya Khlobystov on 23.03.2020.
+//  Copyright © 2020 Ilya Khlobystov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
